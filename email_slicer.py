a259541f83b37f2a7a05481d3c5ba7010de9cc28
Prudence Malatjie <prudentmalatjie@gmail.com>
2025-04-21 22:23:12 +0200

    Update email_slicer.py

--- email_slicer.py
+++ email_slicer.py
@@ -1,7 +1,7 @@
 def email(email):
     username = email[:email.index("@")] # Use string method index within indexing
     domain = email[email.index("@") + 1:]  # email[x + 1: 0]
-    print(f"Your username is {username}")
-    print(f"Your domain is {domain}")
+    print(f"Username: {username}")
+    print(f"Domain: {domain}")
 
 email(email = "raffiki@gmail.com")
